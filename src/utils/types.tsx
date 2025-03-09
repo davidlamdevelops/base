da6062e78fcfeee5173e032b30bf268b36e7f2f4
= <=>
2025-03-09 14:15:44 -0400

    feat(*): additional types

--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -8,10 +8,39 @@ export type Hey = {
   hey?: boolean
 }
 
+const someObj = {
+  key1: 'hey',
+  key2: 'hey 2'
+}
+
 export type Something = {
-  fn: (arg: string) => void,
-  color: (typeof EVENT_COLORS)[number],
+  fn: (arg: string) => void
+  color: (typeof EVENT_COLORS)[number]
+  someVal: typeof someObj
 } & { another?: (arg: string[]) => void} 
   | { another2?: (arg: boolean[]) => void}
   | { another3: UnionOmit<Hey, "id"> }
+
+type ResponseWithStatus<T> = {
+  status: number,
+  data?: T & { error?: string }
+}
+  
+export type Invitation = {
+  source: string
+  status: 'GENERATED' | 'NOT GENERATED' | 'UNDONE'
+  session?: ResponseWithStatus<unknown>
+}
+
+export type Another = Something | Hey | Invitation
+
+export type DynamicDeal = {
+  dealType: 'DYNAMIC PRICE'
+  price: number
+  invitationNoData: Omit<Invitation, 'data'>
+}
+
+export type Keys = 'COMPANY_INFO' | 'CARD_INFO'
+export type Values = 'IN_PROGRESS' | 'COMPLETE' | 'NOT_STARTED'
+export type StepProgress = Partial<Record<Keys, Values>> // Partial makes all properties in T (if Partial<T>) optional
 
